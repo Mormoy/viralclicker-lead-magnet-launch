86be36da464cdc6a8ed59269d9ae37192966362d
Mormoy <moromoyllc@gmail.com>
2025-05-13 22:05:31 -0400

    Update video-player.tsx

--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -14,8 +14,8 @@ const VideoPlayer = ({ onVideoEnd }: VideoPlayerProps) => {
   
   // Video de ejemplo de YouTube (URL pública y confiable)
   // const videoUrl = "https://www.w3schools.com/html/mov_bbb.mp4";
-  // const videoUrl = "https://youtu.be/eKHL93PYDO4";
-  const src = "https://youtu.be/eKHL93PYDO4" ;
+  const videoUrl = "https://www.youtube.com/watch?v=eKHL93PYDO4";
+  
   
   useEffect(() => {
     const video = videoRef.current;
